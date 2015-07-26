bcb7c7bed03769ea459bc60d1f6952903eb2c7b3
Pradeep1321 <npkr9@hotmail.com>
2015-07-26 16:53:08 -0700

    learning

---
+++ IntroductiontoCsharp/Chapter11-4.cs
@@ -0,0 +1,86 @@
+﻿// Pass a derived class reference to a base class reference.
+using System;
+class TwoDShape2 {
+    double pri_width;
+    double pri_height;
+    // Default constructor.
+    public TwoDShape2() {
+        Width = Height = 0.0;
+    }
+    // Constructor for TwoDShape.
+    public TwoDShape2(double w, double h) {
+        Width = w;
+        Height = h;
+    }
+    // Construct object with equal width and height.
+    public TwoDShape2(double x) {
+        Width = Height = x;
+    }
+
+    // Construct a copy of a TwoDShape object.
+    public TwoDShape2(TwoDShape2 ob) {
+        Width = ob.Width;
+        Height = ob.Height;
+    }
+    // Properties for Width and Height.
+    public double Width {
+        get { return pri_width; }
+        set { pri_width = value < 0 ? -value : value; }
+    }
+    public double Height {
+        get { return pri_height; }
+        set { pri_height = value < 0 ? -value : value; }
+    }
+    public void ShowDim() {
+        Console.WriteLine("Width and height are " +
+        Width + " and " + Height);
+    }
+}
+// A derived class of TwoDShape for triangles.
+class Triangle2 : TwoDShape2 {
+    string Style;
+    // A default constructor.
+    public Triangle2() {
+        Style = "null";
+    }
+        // Constructor for Triangle.
+    public Triangle2(string s, double w, double h) : base(w, h) {
+        Style = s;
+    }
+    // Construct an isosceles triangle.
+    public Triangle2(double x) : base(x) {
+        Style = "isosceles";
+    }
+    // Construct a copy of a Triangle object.
+    public Triangle2(Triangle2 ob) : base(ob) {
+        Style = ob.Style;
+    }
+    // Return area of triangle.
+    public double Area() {
+        return Width * Height / 2;
+    }
+
+    // Display a triangle's style.
+    public void ShowStyle()
+    {
+        Console.WriteLine("Triangle is " + Style);
+    }
+}
+class Shapes7
+{
+    static void Main13()
+    {
+        Triangle2 t1 = new Triangle2("right", 8.0, 12.0);
+        // Make a copy of t1.
+        Triangle2 t2 = new Triangle2(t1);
+        Console.WriteLine("Info for t1: ");
+        t1.ShowStyle();
+        t1.ShowDim();
+        Console.WriteLine("Area is " + t1.Area());
+        Console.WriteLine();
+        Console.WriteLine("Info for t2: ");
+        t2.ShowStyle();
+        t2.ShowDim();
+        Console.WriteLine("Area is " + t2.Area());
+    }
+}
---
+++ IntroductiontoCsharp/Chapter11-6.cs
@@ -0,0 +1,118 @@
+﻿
+// Create an abstract class.
+using System;
+abstract class TwoDShape21 {
+    double pri_width;
+    double pri_height;
+    // A default constructor.
+    public TwoDShape21() {
+        Width = Height = 0.0;
+        name = "null";
+    }
+
+    // Parameterized constructor.
+    public TwoDShape21(double w, double h, string n) {
+        Width = w;
+        Height = h;
+        name = n;
+    }
+    // Construct object with equal width and height.
+    public TwoDShape21(double x, string n) {
+        Width = Height = x;
+        name = n;
+    }
+    // Construct a copy of a TwoDShape object.
+    public TwoDShape21(TwoDShape21 ob) {
+        Width = ob.Width;
+        Height = ob.Height;
+        name = ob.name;
+    }
+    // Properties for Width and Height.
+    public double Width {
+        get { return pri_width; }
+        set { pri_width = value < 0 ? -value : value; }
+    }
+
+    public double Height {
+        get { return pri_height; }
+        set { pri_height = value < 0 ? -value : value; }
+    }
+    public string name { get; set; }
+    public void ShowDim() {
+        Console.WriteLine("Width and height are " +
+        Width + " and " + Height);
+    }
+    // Now, Area() is abstract.
+    public abstract double Area();
+}
+
+// A derived class of TwoDShape for triangles.
+class Triangle21 : TwoDShape21 {
+    string Style;
+    // A default constructor.
+    public Triangle21() {
+        Style = "null";
+    }
+    // Constructor for Triangle.
+    public Triangle21(string s, double w, double h) :
+        base(w, h, "triangle") {
+        Style = s;
+    }
+
+        // Construct an isosceles triangle.
+    public Triangle21(double x) : base(x, "triangle") {
+        Style = "isosceles";
+    }
+    // Construct a copy of a Triangle object.
+    public Triangle21(Triangle21 ob) : base(ob) {
+        Style = ob.Style;
+    }
+    // Override Area() for Triangle.
+    public override double Area() {
+        return Width * Height / 2;
+    }
+    // Display a triangle's style.
+    public void ShowStyle() {
+        Console.WriteLine("Triangle is " + Style);
+    }
+}
+// A derived class of TwoDShape for rectangles.
+class Rectangle21 : TwoDShape21 {
+    // Constructor for Rectangle.
+    public Rectangle21(double w, double h) :
+        base(w, h, "rectangle") { }
+    // Construct a square.
+    public Rectangle21(double x) :
+        base(x, "rectangle") { }
+    // Construct a copy of a Rectangle object.
+    public Rectangle21(Rectangle21 ob) : base(ob) { }
+    // Return true if the rectangle is square.
+    public bool IsSquare()
+    {
+        if (Width == Height) return true;
+        return false;
+    }
+    // Override Area() for Rectangle.
+    public override double Area()
+    {
+        return Width * Height;
+    }
+}
+
+class AbsShape
+{
+    static void Main()
+    {
+        TwoDShape21[] shapes = new TwoDShape21[4];
+        shapes[0] = new Triangle21("right", 8.0, 12.0);
+        shapes[1] = new Rectangle21(10);
+        shapes[2] = new Rectangle21(10, 4);
+        shapes[3] = new Triangle21(7.0);
+        for (int i = 0; i < shapes.Length; i++)
+        {
+            Console.WriteLine("object is " + shapes[i].name);
+            Console.WriteLine("Area is " + shapes[i].Area());
+            Console.WriteLine();
+        }
+    }
+}
