5e2b5325925f55facda5c926e7345a3a12d8405f
Pradeep1321 <npkr9@hotmail.com>
2015-07-26 16:51:43 -0700

    learning

---
+++ IntroductiontoCsharp/Chapter11-7-Sealed.cs
@@ -0,0 +1,22 @@
+﻿/*
+To prevent a class from being inherited, precede its declaration with sealed. As you
+might expect, it is illegal to declare a class as both abstract and sealed because an abstract
+class is incomplete by itself and relies upon its derived classes to provide complete
+implementations.
+
+*/
+
+class B1
+{
+    public virtual void MyMethod() { /* ... */ }
+}
+class D1 : B1
+{
+    // This seals MyMethod() and prevents further overrides.
+    sealed public override void MyMethod() { /* ... */ }
+}
+class X1 : D1
+{
+    // Error! MyMethod() is sealed!
+    //public override void MyMethod() { /* ... */ }
+}
---
+++ IntroductiontoCsharp/Chapter11-8-Object.cs
@@ -0,0 +1,56 @@
+﻿/*
+C# defines one special class called object that is an implicit base class of all other classes and
+for all other types (including the value types). In other words, all other types are derived
+from object. 
+
+The object class defines the methods shown in Table 11-1, which means that they are
+available in every object.
+Method                                                    Purpose           
+public virtual bool Equals(object obj)                  Determines whether the invoking object is the
+                                                        same as the one referred to by obj.
+public static bool Equals(object objA, object objB)     Determines whether objA is the same as objB.
+                                                        protected virtual Finalize( ) Performs shutdown actions prior to garbage
+                                                        collection. In C#, Finalize( ) is accessed through
+                                                        a destructor.
+public virtual int GetHashCode( )                       Returns the hash code associated with the
+                                                        invoking object.
+public Type GetType( )                                  Obtains the type of an object at runtime.
+                                                        protected object MemberwiseClone( ) Makes a “shallow copy” of the object. This
+                                                        is one in which the members are copied, but
+                                                        objects referred to by members are not.
+public static bool ReferenceEquals(object objA,object objB)       Determines whether objA and objB refer to the same object
+public virtual string ToString( )                       Returns a string that describes the object.
+
+ * 
+ * 
+ * 
+*/
+
+// Demonstrate ToString()
+using System;
+class MyClass1
+{
+    static int count = 0;
+    int id;
+    public MyClass1()
+    {
+        id = count;
+        count++;
+    }
+    public override string ToString()
+    {
+        return "MyClass object #" + id;
+    }
+}
+class Test1
+{
+    static void Main()
+    {
+        MyClass1 ob1 = new MyClass1();
+        MyClass1 ob2 = new MyClass1();
+        MyClass1 ob3 = new MyClass1();
+        Console.WriteLine(ob1);
+        Console.WriteLine(ob2);
+        Console.WriteLine(ob3);
+    }
+}
